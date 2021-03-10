726085e719e0f0aeb052d80edc6d15836c45973c
Dylan <dylanmgaines1@gmail.com>
2021-03-10 18:55:49 -0500

    Inch By Inch
    > Set up temporary article.html page
    >> currently uses card as main container, may change to more appropriate one
    >> lorem ipsum is placeholder for content
    > set admin/index.html to admin/admindex.html to avoid confusion !!CHANGE ON SUBMIT
    > moved card stuff from css to index.html, may move back if container is switched from card
    > moved js/q for nav bar into separate document to avoid redundancy
    >set up placeholders for all pages
    >> navbar, footer, card article at center
    >>assumes not signed in, MUST BE CHANGED FOR ADMIN
    
    ! mess with article,
    \-- fine tune appearance
    \--add ads because we all gotta hustle
    \-- where the HECK does the image go?
    ! eat, you fool
    
    Post-that
    ! JSON blob

---
+++ shrinkerAndSigner.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+    $(".nav-item-list").click(function() {
+        alert("it work");
+    });
+    $("#contact").click(function() {
+        alert("go away");
+    });
+})
+$(document).on("scroll", function() {
+    if ($(document).scrollTop() > 85) {
+        $(".primary-nav").addClass("shrink");
+        $(".title").addClass("display-5");
+        $(".bi-journal-code").addClass("shrunk");
+        $(".navbar-toggler-icon").addClass("icon-shrink");
+    } else {
+        $(".primary-nav").removeClass("shrink");
+        $(".title").removeClass("display-5");
+        $(".bi-journal-code").removeClass("shrunk");
+        $(".navbar-toggler-icon").removeClass("icon-shrink");
+    }
+});
+
+//temp until actual server-side validation is done
+function subIt() {
+    var un, pw;
+    un = $("#player").val().trim();
+    pw = $("#key").val().trim();
+    if (un == "dylan" && pw == "pw") {
+        $("#theBlackDoor").attr("hidden", true);
+        $("#loader").attr("hidden", false);
+        $("#tester").addClass("theHandPrint");
+        $("#isValid").text("welcome home");
+        /*
+        setTimeout(() => {
+            location = "admin/index.html";
+        }, 2000);
+        */
+    } else {
+        $("#isValid").text("you are not worthy");
+    }
+}
