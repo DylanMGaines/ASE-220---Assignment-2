f82528832c3f70e30cceca1dfba82b19d8a314e1
Dylan <dylanmgaines1@gmail.com>
2021-03-13 22:23:17 -0500

    The Facelift
    We've done it, boys.
    > index.html and article.html now work entirely dynamically via AJAX and JSON blob.
    > both of their scripts have been cleaned up and consolidated into their loader.js classes
    > minor changes to templates.json to enhance functionality and design
    
    From here, the plan is simple, it practically completes itself, so BOYS:
    
    ! update admin.html

--- iJS/articleLoader.js
+++ iJS/articleLoader.js
@@ -1,22 +1,43 @@
-/*function letsRock() {
-    let imgSrc = 'media/download.jpg';
+//nameTag, subtitle, likes, imgCap, content pathToImg, desc, dateMade,  
+const bitsNPieces = ['#title', '#subtitle', '.counter', '.figure-caption', '#qualityContent'];
 
-    //let timeTrial = '1/1/2021';
-    let $htmlString = $("#templart").clone(false).contents();
-    $('.figure-img', $htmlString).attr('src', imgSrc);
-    $('#title', $htmlString).append(nameTag);
-    $('#subtitle', $htmlString).append(minilad);
-    $('#qualityContent', $htmlString).append(contento);
-    $('.figure-caption', $htmlString).append(imgCap);
-    $('#underCount', $htmlString).append(counter);
-    $('#counter', $htmlString).append(counter);
+function letsRock() {
+    var aNum = window.location.search.substring(3);
+    $.getJSON("https://jsonblob.com/api/5df95c1f-8374-11eb-a0d4-a5d78bdc5d78/", function(data) {
+        //initial load (loads 6)
+        let template;
+        $.getJSON("./assets/templates.json", function(templates) {
+            template = templates.article;
+            letsRoll(data[aNum], template);
+        });
+    });
+    $.getJSON("./assets/modals.json", function(modals) {
+        $(".bg-body").append(modals.signInModal);
+    });
+}
+
+function letsRoll(arItem, templateString) {
+    let $htmlString = $(templateString).clone(true);
+
+    let i = 0;
+    for (x in arItem) {
+        $(bitsNPieces[i], $htmlString).append(arItem[x]);
+        if (i == 4) { break; }
+        i++;
+    }
+
+    $('.figure-img', $htmlString).attr('src', arItem.pathToImg);
+    let dateMade = new Date(arItem['dateMade']);
+    $('#dateMade', $htmlString).append((dateMade.getMonth() + 1) + '/' + dateMade.getDate() + '/' + dateMade.getFullYear());
+    let timeTrial = Math.round(Math.abs(((new Date().getTime()) - (dateMade).getTime()) / (24 * 60 * 60 * 1000)));
+    $('#datePubd', $htmlString).append('Last Updated ' + timeTrial + ' days ago');
     $('#testHolder').before($htmlString);
-}*/
+}
 
 function smash(whichOne) {
     var theOne = (whichOne == 'B') ? $("#thatLikeButtonB") : $("#thatLikeButtonT");
     var theOther = (whichOne == 'T') ? $("#thatLikeButtonB") : $("#thatLikeButtonT");
-    var cVal = parseInt($('#counter').text());
+    var cVal = parseInt($('#countPrime').text());
     console.log(cVal);
     if (theOne.hasClass('active') == true) {
         cVal++;
@@ -25,8 +46,7 @@ function smash(whichOne) {
     }
     console.log(cVal);
     theOther.toggleClass("active");
-    $('#counter').text(cVal);
-    $('#underCount').text(cVal);
+    $('.counter').text(cVal);
     theOne.blur();
     theOther.blur();
 }
--- iJS/cardLoader.js
+++ iJS/cardLoader.js
@@ -1,4 +1,4 @@
-//nameTag, subtitle, desc, dateMade, likes, imgCap, content
+//nameTag, subtitle, pathToImg, desc, dateMade, likes, imgCap, content
 const bitsNPieces = ['.card-title', '.card-text', '.text-muted']; //title, subtitle, date updated
 
 function cardLoader(data, accessing, templateString) {
@@ -14,6 +14,35 @@ function cardLoader(data, accessing, templateString) {
     let timeTrial = Math.round(Math.abs(((new Date().getTime()) - (dateMade).getTime()) / (24 * 60 * 60 * 1000)));;
     $('.card-img-top', $htmlString).attr('src', accessed.pathToImg);
     $('.text-muted', $htmlString).append('Last Updated ' + timeTrial + ' days ago'); //make dynamic time unit (eg days instead of weeks) selector
-    $('.card', $htmlString).wrap("<a href='article.html' class='text-decoration-none text-body'></a>")
+    $('.card', $htmlString).wrap("<div onclick='reqTime(" + accessing + ")' class='text-decoration-none text-body' style='cursor: pointer;'></div>");
     $('#testHolder').before($htmlString);
+}
+
+function placehold() {
+    var currentLoaded;
+    $.getJSON("https://jsonblob.com/api/5df95c1f-8374-11eb-a0d4-a5d78bdc5d78/", function(data) {
+        //initial load (loads 6)
+        let template;
+        $.getJSON("./assets/templates.json", function(templates) {
+            template = templates.card;
+            for (currentLoaded = 0; currentLoaded < data.length && currentLoaded < 6; currentLoaded++) {
+                cardLoader(data, (data.length - (currentLoaded + 1)), template);
+            }
+            //load button clicked (loads 6 more)
+            $("#loadButton").click(function() {
+                for (; currentLoaded < data.length && currentLoaded < 6; currentLoaded++) {
+                    cardLoader(data, currentLoaded);
+                    console.log(data[currentLoaded]);
+                }
+            });
+        });
+    });
+    $.getJSON("./assets/modals.json", function(modals) {
+        $(".bg-body").append(modals.signInModal);
+    });
+}
+
+function reqTime(aNum) {
+    console.log(aNum);
+    window.location.href = './article.html?a=' + aNum;
 }
